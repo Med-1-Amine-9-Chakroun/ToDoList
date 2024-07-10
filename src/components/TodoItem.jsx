f3bec8d8ddc63c677c354f0643a7fb1f364ec07c
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-10 10:44:52 +0100

    Delete functionality completed

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,21 @@
 import styles from "./todoitem.module.css";
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    console.log(item);
+    setTodos(todos.filter((todoitem) => todoitem !== item));
+    console.log(todos);
+  }
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
         {item}
         <span>
-          <button className={styles.deleteButton}>x</button>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
         </span>
       </div>
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,10 @@
 import TodoItem from "./TodoItem";
 import styles from "./todoList.module.css";
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
   return (
     <div className={styles.list}>
       {todos.map((item) => (
-        <TodoItem item={item} />
+        <TodoItem item={item} todos={todos} setTodos={setTodos} />
       ))}
     </div>
   );
